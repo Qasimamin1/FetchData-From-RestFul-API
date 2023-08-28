a210310e3656595782da3c9092c711be35bb5f93
Qasim Amin <qasim.amin@afry.com>
2023-08-28 13:06:53 +0200

    first commit

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContactsMvcApp.Services;
+using System.Threading.Tasks;
+
+namespace ContactsMvcApp.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public ContactsController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var contacts = await _apiService.GetContactsAsync();
+            return View(contacts);
+        }
+    }
+}
---
+++ Services/ApiService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using ContactsMvcApp.Models;
+using System.Collections.Generic;
+
+namespace ContactsMvcApp.Services
+{
+    //The ApiService class provides functionalities to communicate with an external API.
+    //It encapsulates the logic for making HTTP requests and processing responses, specifically to fetch contacts.
+
+    public class ApiService
+    {
+        //HttpClient, which is the main class used for sending HTTP requests and receiving responses.
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        //asynchronous method that returns a list (IEnumerable) of Contact objects.
+        public async Task<IEnumerable<Contact>> GetContactsAsync()
+        {
+
+            //The response from the server is stored in the response variable.
+            var response = await _httpClient.GetAsync("/api/Contacts");
+
+            //asynchronous method that returns a list (IEnumerable) of Contact objects.
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Contact>>();
+        }
+    }
+}
